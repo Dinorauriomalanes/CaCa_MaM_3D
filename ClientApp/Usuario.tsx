00d90b36871b0afd40509f318e2476a761719d42
Dinorauriomalanes <daniel.barronagundis.gen23@cbtis105.edu.mx>
2025-06-11 22:30:38 -0600

    raiz

--- ClientApp/Usuario.tsx
+++ ClientApp/Usuario.tsx
@@ -1,4 +1,35 @@
+import { useEffect, useState } from "react"
+
+type Usuario = {
+    "Id": string,
+    "Nombre": string,
+    "Correo": string,
+    "Password": string
+}
 const Usuario = () => {
+    const [registro, setRegistros] = useState<Usuario[]>([]);
+    const [texto, setTexto] = useState("");
+
+    const listarRegistro = async () => {
+        let url = "/api/usuarios";    
+        if(texto){
+            url += "?texto=" + texto;
+        }
+
+        const resp = await fetch(url);
+        if(resp.ok){
+            const datos = await resp.json();
+            setRegistros(datos);
+        }
+    }
+
+    useEffect(()=>{
+        listarRegistro();
+    }, []);
+
+    const Buscar = () => {
+        listarRegistro();
+    }
     return(
         <>
         <div className="container">
@@ -12,13 +43,60 @@ const Usuario = () => {
                         <div className="col-12">
                             <div className="mb-3">
                                 <label>Busqueda de Usuarios</label>
-                                <input type="text" className="form-control" placeholder="Introduce el nombre o el correo" />
+                                <input type="text" className="form-control" 
+                                placeholder="Introduce el nombre o el correo"
+                                onChange={(e) => setTexto(e.target.value)} 
+                                />
                             </div>
                         </div>
                         <div className="col-12">
-                            <button className="btn btn-primary">Buscar</button>
+                            <button className="btn btn-primary" onClick={Buscar}>Buscar</button>
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="container mt-4">
+            <div className="card">
+                <div className="card-header">Usuarios Existentes</div>
+                <div className="card-body">
+                    <table className="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>Nombre</th>
+                                <th>Correo</th>
+                                <th>Password</th>
+                            </tr>
+                        </thead>
+                        {
+                            registro.length === 0 &&
+                            <tbody>
+                                <tr>
+                                    <td colSpan={5}>No hay registros para mostrar</td>
+                                </tr>
+                            </tbody>
+                        }
+                        {
+                            registro.length > 0 &&
+                            <tbody>
+                                {
+                                    registro.map((item, index) =>
+                                    <tr>
+                                        <td>{index + 1}</td>
+                                        <td>{item.Nombre}</td>
+                                        <td>{item.Correo}</td>
+                                        <td>{item.Password}</td>
+                                        <td className="d-flex gap-2">
+                                            <a className="btn btn-primary" href={"/usuarios/" + item.Id}>Editar</a>
+                                            <button className="btn btn-danger">Eliminar</button>
+                                        </td>
+                                    </tr>)
+                                }
+                            </tbody>
+                        }
+                    </table>
                 </div>
             </div>
         </div>
